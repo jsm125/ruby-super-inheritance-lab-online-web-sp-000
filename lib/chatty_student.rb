a52a2c8e9d807e415e751b8368ebb7d2feaebbee
Matt Im <matthew.seiim@gmail.com>
2020-05-13 21:25:54 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,11 +7,8 @@ class ChattyStudent < Student
     
     def raise_hand
       super
-        puts "Pick me!"
-      super
-        puts "Pick me!"
-      super
-        puts "Pick me!"        
+        puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+      end
 
     end
 end
